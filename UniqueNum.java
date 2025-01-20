ada8d52ef5d1d0e14aee98a368dab24b09f26b0b
PushOnWork <cooljoy120@gmail.com>
2025-01-20 18:21:42 +0530

    Create UniqueNum.java

---
+++ UniqueNum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class NumDude{
+    public static boolean nonRepeat(int n){
+        int arr[]=new int [10];
+        while(n!=0){
+            int d;
+            d=n%10;
+            arr[d]++;
+            n=n/10;
+        }
+        int num=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=2){
+                num=1;
+            }
+        }
+        return (num!=1);
+    }
+    public static boolean Unique(int n){
+        return(nonRepeat(n));
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter max");
+        int m=sc.nextInt();
+        System.out.println("Enter min");
+        int n=sc.nextInt();
+        int c=0;
+        System.out.println("THE UNIQUE-DIGIT INTEGERS ARE:");
+        for(int i=n;i<=m;i++){
+            if(Unique(i)){
+                System.out.print(" "+i);
+                c++;
+            }
+        }
+        System.out.println();
+        if (c!=0)
+        System.out.println("THE UNIQUE-DIGIT INTEGERS ARE: "+c);
+        else
+        System.out.println("THE UNIQUE-DIGIT INTEGERS ARE: Nil");
+    }
+}
